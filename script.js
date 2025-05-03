0765868d1ad414efda77c1bac6a54247f618b134
Giovanni Baldo <giibola21@gmail.com>
2025-05-02 21:01:40 -0300

    arrumar casas de numero

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ class parquimetro{
     }    
       trocoFinal(){
         this.trocoFinal = this.valorInserido - this.valorSerpago;
-        document.getElementById('exibirtroco').innerText = "O valor de troco sera : R$" + this.trocoFinal;
+        document.getElementById('exibirtroco').innerText = "O valor de troco sera : R$" + this.trocoFinal.toFixed(2);
         console.log(this.trocoFinal);
       }
       showHoras(){
